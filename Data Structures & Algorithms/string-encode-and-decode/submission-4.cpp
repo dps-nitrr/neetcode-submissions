6dbb62760b0b32ca828d0cef19e97f6a1d7bf302
dps-nitrr <sisodiyadivyapratap@gmail.com>
2026-08-15 11:47:13 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string temp="";
+        for(int i=0;i<strs.size();i++){
+            temp+=to_string(strs[i].size()) +'#'+strs[i];
+        }
+        
+        
+        return temp;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>ans;
+       int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#')j++;
+
+            int len = stoi(s.substr(i, j - i));
+ 
+j++;
+            ans.push_back(s.substr(j, len));
+            i=j+len;
+        }
+        return ans;
+
+    }
+};
