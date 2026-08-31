0f2513b1e9d80ac402716ba9785af61a4d2acac9
dps-nitrr <sisodiyadivyapratap@gmail.com>
2026-08-31 23:29:25 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void f(int i, vector<int>& nums, vector<vector<int>>&ans){
+        if(i==nums.size()){ans.push_back(nums); return ;}
+
+        for(int idx=i;idx<nums.size();idx++){
+            swap(nums[i], nums[idx]);
+            f(i+1, nums, ans);
+            swap(nums[i], nums[idx]);
+        }
+
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        f(0, nums, ans);
+        return ans;
+    }
+};
